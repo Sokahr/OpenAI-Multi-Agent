3d09b97fc160d912f3dd8b25f7eb7da43373fd1f
Benjamin Peter <ben61279@gmail.com>
2023-07-11 12:06:54 +0200

    Fixed linting issues in test_main_menu.py

--- tests/test_main_menu.py
+++ tests/test_main_menu.py
@@ -1,3 +1,8 @@
+"""
+This module contains tests for 
+the main menu functionality of the OpenAI MAC application.
+"""
+
 import pytest
 from click.testing import CliRunner
 from src.main_menu import MainMenu
@@ -9,6 +14,7 @@ def runner():
 
 
 def test_display_menu(runner):
+    """Test the display_menu() method of the MainMenu class."""
     main_menu = MainMenu()
     result = runner.invoke(main_menu.display_menu)
     assert result.exit_code == 0
@@ -16,6 +22,7 @@ def test_display_menu(runner):
 
 
 def test_start_new_project(runner):
+    """Test the start_new_project() method of the MainMenu class."""
     main_menu = MainMenu()
     result = runner.invoke(main_menu.start_new_project)
     assert result.exit_code == 0
@@ -23,6 +30,7 @@ def test_start_new_project(runner):
 
 
 def test_invalid_option(runner):
+    """Test the execute_command() method of the MainMenu class with an invalid option."""
     main_menu = MainMenu()
     result = runner.invoke(main_menu.execute_command, ["5"])
     assert result.exit_code == 0
